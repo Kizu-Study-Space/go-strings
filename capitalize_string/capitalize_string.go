810fb085338af742952000a9b5b968734b0043d7
Zora Fuchs <13620981+zorafuchs@users.noreply.github.com>
2023-08-23 04:50:18 +0000

    Refactor capitalize_string by using ranged access to string indexes

--- capitalize_string/capitalize_string.go
+++ capitalize_string/capitalize_string.go
@@ -2,7 +2,5 @@ package capitalize_string
 import "strings"
 
 func CapitalizeString(string_to_capitalize string) string {
-	capitalized_string := strings.ToUpper(string(string_to_capitalize[0]))
-	capitalized_string += strings.TrimPrefix(string_to_capitalize, string(string_to_capitalize[0]))
-	return capitalized_string
+	return strings.ToUpper(string_to_capitalize[0:1]) + string_to_capitalize[1:len(string_to_capitalize)]
 }
