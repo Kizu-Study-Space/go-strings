832dc4883bf11107a6227a0d8fa0c60919ed3cf0
Zora Fuchs <13620981+zorafuchs@users.noreply.github.com>
2023-08-23 04:53:41 +0000

    Refactor reverse_string by removing unnecessary output

--- reverse_string/reverse_string.go
+++ reverse_string/reverse_string.go
@@ -1,13 +1,11 @@
 package reverse_string
 
-import "fmt"
-
 func ReverseString(string_to_reverse string) string {
 	reversed_string := ""
 
 	for current_index := len(string_to_reverse) - 1; current_index >= 0; current_index-- {
 		reversed_string += string(string_to_reverse[current_index])
 	}
-	fmt.Printf("%s\n", reversed_string)
+
 	return reversed_string
 }
